f50bbdb4c904074cf227eac16f1f614d14f070fb
bharat jagoar <bharatjagoar15@gmail.com>
2023-08-28 22:35:08 +0530

    problem__1207

--- LECTURE10/problem1207.cpp
+++ LECTURE10/problem1207.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main(){
     map<int,int> bharat;
-    int arr[] = {1,2,2,1,1,3};
-    int size = sizeof(arr)/sizeof(int);
+    // int arr[] = {-3,0,1,-3,1,1,1,-3,10,0};
+    // int size = sizeof(arr)/sizeof(int);
 
     for (int i = 0; i < size; i++)
     {
@@ -18,10 +18,22 @@ int main(){
             bharat[arr[i]]=bharat[arr[i]]+1;
         }
     }
-    
-    for (auto x=bharat.begin();x!=bharat.end();++x){
-        // cout<< x->first<<endl;
+    for (auto i = bharat.begin(); i !=bharat.end(); ++i)
+    {
         int count=0;
+        // cout<<i->first<<"   "<<i->second<<endl;
+        for (auto j = i; j !=bharat.end(); ++j)
+        {
+            // cout<<i->first<<"   "<<i->second<<"\t"<<j->first<<"    "<<j->second<<endl;
+            if(i->second==j->second){
+                count++;
+            }
+        }
+        if(count>1){
+            cout<<"not unique occurence"<<endl;
+            break;
+        }
     }
-    return 1;
+    
+    return 0;
 }
